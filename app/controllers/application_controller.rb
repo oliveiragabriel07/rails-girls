fbcf714fbb460bf05a104f457fad3c9efeb695be
Gabriel Oliveira <oliveira.gabriel07@gmail.com>
2015-04-08 15:59:36 -0300

    Adicionando campos birth_date, first_name e last_name no model User
    Signed-off-by: Gabriel Oliveira <oliveira.gabriel07@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+		u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+	end
+
+	devise_parameter_sanitizer.for(:account_update) do |u|
+    	u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 end
