2f3307778f4f211290666e68019f2f0bf78e98b7
Gabriel Oliveira <oliveira.gabriel07@gmail.com>
2015-04-08 16:03:20 -0300

    Adicionando referencia do user em ideas
    Signed-off-by: Gabriel Oliveira <oliveira.gabriel07@gmail.com>

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ActiveRecord::Base
+	belongs_to :user
 	has_many :comments
 	mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407143709) do
+ActiveRecord::Schema.define(version: 20150408181458) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20150407143709) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "ideas", ["user_id"], name: "index_ideas_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -42,6 +45,9 @@ ActiveRecord::Schema.define(version: 20150407143709) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "birth_date"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
